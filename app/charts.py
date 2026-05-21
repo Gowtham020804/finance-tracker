bd3f1e172e5896c6a1f734fd4d874987b6249173
Gowtham Mekala <151367479+Gowtham020804@users.noreply.github.com>
2026-05-22 02:54:01 +0530

    Update charts.py

--- app/charts.py
+++ app/charts.py
@@ -1,13 +1,14 @@
-import streamlit as st 
-import plotly.express as px 
- 
-def show_charts(df): 
- 
-    pie = px.pie(df, names='Category', values='Amount') 
-    st.plotly_chart(pie) 
- 
-    bar = px.bar(df, x='Category', y='Amount') 
-    st.plotly_chart(bar) 
- 
-    line = px.line(df, y='Amount') 
+import streamlit as st
+import plotly.express as px
+
+
+def show_charts(df):
+
+    pie = px.pie(df, names="Category", values="Amount")
+    st.plotly_chart(pie)
+
+    bar = px.bar(df, x="Category", y="Amount")
+    st.plotly_chart(bar)
+
+    line = px.line(df, y="Amount")
     st.plotly_chart(line)
